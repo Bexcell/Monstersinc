7e8451e814c6ce8292e337c9824ed03cf0779e56
Brittani Excell <Brittani.Excell@yahoo.com>
2019-08-24 01:20:40 -0400

    cards-done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
   componentDidMount() {
@@ -16,9 +18,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => {
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            );
+          }}
+        />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
