0ee086f152392dc095114a8cb05805a4dc89d965
Brittani Excell <Brittani.Excell@yahoo.com>
2019-09-20 00:30:23 -0400

    done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       searchField: ""
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -29,6 +29,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={e => this.setState({ searchField: e.target.value })}
