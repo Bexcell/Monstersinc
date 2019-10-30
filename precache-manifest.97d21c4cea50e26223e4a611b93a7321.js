a421d61d00ad0a3f8b39cb976638720ceea353a8
Brittani Excell <Brittani.Excell@yahoo.com>
2019-10-29 20:29:33 -0400

    Updates

---
+++ precache-manifest.97d21c4cea50e26223e4a611b93a7321.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833225fbab9136a7378eaff83470f59c",
+    "url": "/Monstersinc/index.html"
+  },
+  {
+    "revision": "1a5f531a246c3a03bd3c",
+    "url": "/Monstersinc/static/css/main.9864a221.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/Monstersinc/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "1a5f531a246c3a03bd3c",
+    "url": "/Monstersinc/static/js/main.b057faa0.chunk.js"
+  },
+  {
+    "revision": "6fb294efb8319e6fac65",
+    "url": "/Monstersinc/static/js/runtime~main.f6ad8d37.js"
+  }
+]);
